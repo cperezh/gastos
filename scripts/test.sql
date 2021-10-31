b371f29f78f7694643f5ea12efb6c94f5e4551b9
carlos <cperezh@gmail.com>
2021-10-31 13:28:22 +0100

    Refactor para incluir el saldo como PK de un movimiento

--- scripts/test.sql
+++ scripts/test.sql
@@ -2,6 +2,7 @@
 truncate table staging.movimientos;
 truncate table staging.subcategoria;
 truncate table staging.dia;
+truncate table staging.gastos_fact;
 truncate table public.gastos_fact;
 delete from public.subcategoria;
 delete from public.dia;
@@ -19,17 +20,18 @@ select count(*) from staging.subcategoria, staging.dia, staging.gastos_fact;
 -- Inserto gasto con mismas dimensiones. No debe duplicar las dimensiones
 -- Inserto gasto con dias nulo e invalido. Deba acabar con dia_key = 0 y -1
 -- Inserto gasto con subcategora o categoria NULL. Debe acabar con subcategoria_key = 0
-insert into staging.movimientos(importe, fvalor, categoria, subcategoria) values (1000, '2021-01-02', 'CAT1', 'SUBCAT1');
-insert into staging.movimientos(importe, fvalor, categoria, subcategoria) values (2000, '2021-01-03', 'CAT2', 'SUBCAT2');
-insert into staging.movimientos(importe, fvalor, categoria, subcategoria) values (3000, '2021-01-02', 'CAT1', 'SUBCAT1');
-insert into staging.movimientos(importe, fvalor, categoria, subcategoria) values (4000, NULL, 'CAT4', 'SUBCAT4');
-insert into staging.movimientos(importe, fvalor, categoria, subcategoria) values (6000, '2021-06-01', NULL, 'SUBCAT6');
-insert into staging.movimientos(importe, fvalor, categoria, subcategoria) values (7000, '2021-06-01', 'CAT7', NULL);
+insert into staging.movimientos(importe, saldo, fvalor, categoria, subcategoria) values (-1000, -500, '2021-01-02', 'CAT1', 'SUBCAT1');
+insert into staging.movimientos(importe, saldo, fvalor, categoria, subcategoria) values (-2000, -1500, '2021-01-03', 'CAT2', 'SUBCAT2');
+insert into staging.movimientos(importe, saldo, fvalor, categoria, subcategoria) values (-3000, -2500, '2021-01-02', 'CAT1', 'SUBCAT1');
+insert into staging.movimientos(importe, saldo, fvalor, categoria, subcategoria) values (-4000, -3500, NULL, 'CAT4', 'SUBCAT4');
+insert into staging.movimientos(importe, saldo, fvalor, categoria, subcategoria) values (-6000, -5500, '2021-06-01', NULL, 'SUBCAT6');
+insert into staging.movimientos(importe, saldo, fvalor, categoria, subcategoria) values (-7000, -6500, '2021-06-01', 'CAT7', NULL);
 
-select * from staging.movimientos
+select * from staging.movimientos;
 select * from public.subcategoria;
 select * from public.dia;
 select * from public.gastos_fact;
+select * from staging.gastos_fact;
 
 -- Lanzo otra vez la ETL para ver que no duplica
 -- Vacio staging.movimientos para ver que no borra
@@ -40,7 +42,15 @@ select * from public.gastos_fact
 
 -- Inserto un gasto nuevo, con nuevas dimensiones
 truncate table staging.movimientos
-insert into staging.movimientos(importe, fvalor, categoria, subcategoria) values (8000, '2021-07-01', 'CAT8', 'SUBCAT8');
+insert into staging.movimientos(importe, saldo, fvalor, categoria, subcategoria) values (-8000, -7500, '2021-07-01', 'CAT8', 'SUBCAT8');
 select * from public.subcategoria;
 select * from public.dia;
 select * from public.gastos_fact
+
+-- Cambio un movimiento de categoria
+truncate table staging.movimientos
+insert into staging.movimientos(importe, saldo, fvalor, categoria, subcategoria) values (-8000, -7500, '2021-07-01', 'CAT8_CAMBIO', 'SUBCAT8_CAMBIO');
+select * from public.subcategoria;
+select * from public.dia;
+select * from public.gastos_fact
+
